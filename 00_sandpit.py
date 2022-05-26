b2d7abb94e0e48cba8affce26bafdd8cc9848e82
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-05-26 14:19:24 +1200

    new string checker, doesn't ask question

--- 00_sandpit.py
+++ 00_sandpit.py
@@ -1,15 +1,17 @@
 import re
 
-a = str.split("another one")
-print(a)
+# split the input into a list
+a = str.split("opposite and adjacent")
+# take out the 'and'
+a.remove("and")
+
+# check each item in the list to match with specific side, making true if
+# comparison found
+for item in a:
+    if re.match("adjacent", item):
+        print("hi")
+        adjacent = True
 
 
-
-a = "opposite and adjacent"
-
-regex = "opposite"
-
-if re.match(regex, a):
-    a = str.split(a)
-    a.remove("and")
-print(a)
+# print to check adjacent true
+print(adjacent)
---
+++ 02_string_checker_v3.py
@@ -0,0 +1,47 @@
+# string checker, checks for valid input from mini lists
+def string_checker(response, valid_list, error):
+
+    # compare response with items in list
+    for var_list in valid_list:
+        if response in var_list:
+            response = var_list[0]
+            var_valid = True
+            break
+        else:
+            var_valid = False
+    
+    # if response is found to be valid, return first item in valid list otherwise print error
+    if var_valid == True:
+        return response
+    else:
+        print(error)
+        return "invalid"
+
+# blank checking function, checks if a string input is blank
+def not_blank(question, error):
+    
+    var_loop = True
+    while var_loop:
+        
+    # take in response and check if it is blank
+        response = input(question).lower()
+        
+        if response != "":
+            return response
+        else:
+            print(error)
+
+# main routine
+
+yes_no = [
+    ["yes", "sure", "okay"],
+    ["no"]
+    ]
+    
+for item in range(5):
+    loop = "invalid"
+    while loop == "invalid":
+        ask_question = input("Yes or no? ")
+        test_function = string_checker(ask_question, yes_no, "Please enter yes or no")
+    print("You said {}".format(test_function))
+    print()
