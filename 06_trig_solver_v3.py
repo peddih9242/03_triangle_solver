87611b6c1d48e6f6309e3f1b31f5e1c221469b10
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-05-31 11:00:13 +1200

    almost complete trig solver

--- 06_trig_solver_v3.py
+++ 06_trig_solver_v3.py
@@ -34,23 +34,29 @@ def num_check(question, error, high=None):
 
 
 # string checker, checks for valid input from mini lists
-def string_checker(response, valid_list, error):
-
-    # compare response with items in list
-    for var_list in valid_list:
-        if response in var_list:
-            response = var_list[0]
-            var_valid = True
-            break
-        else:
-            var_valid = False
+def string_checker(question, valid_list, error):
     
-    # if response is found to be valid, return first item in valid list otherwise print error
-    if var_valid == True:
-        return response
-    else:
-        print(error)
-        return "invalid"
+    # loop taking in input and string checking process
+    valid = False
+    while not valid:
+        
+        # take input
+        response = input(question).lower()
+
+        # compare response with items in list
+        for var_list in valid_list:
+            if response in var_list:
+                response = var_list[0]
+                var_valid = True
+                break
+            else:
+                var_valid = False
+        
+        # if response is found to be valid, return first item in valid list otherwise print error
+        if var_valid == True:
+            return response
+        else:
+            print(error)
 
 def trig_answer():
     # use questions to set up future questions
@@ -136,50 +142,98 @@ def new_trig():
     # ask user a series of questions to figure out what calculation type
     # to use and calculate answer
     
-    side_check = "invalid"
-    while side_check == "invalid":
-        two_side = input("Do you have 2 sides? ")
-        side_check = string_checker(two_side, yes_no, "Please enter yes or no.")
+    two_side = string_checker("Do you have 2 sides? ", yes_no, "Please enter yes or no.")
 
     if two_side == "yes":
         
-        pyth_check = "invalid"
-        while pyth_check == "invalid":
-            third_side = input("Do you need to find the third side? ")
-            pyth_check = string_checker(third_side, yes_no, "Please enter yes or no.")
+        third_side = string_checker("Do you need to find the third side? ", yes_no, "Please enter yes or no.")
         
         if third_side == "yes":
 
-            hyp_check = "invalid"
-            while hyp_check == "invalid":
-                getting_hyp = input("Are you given the hypotenuse? ")
-                hyp_check = string_checker(getting_hyp, yes_no, "Please enter yes or no.")
-
+            getting_hyp = string_checker("Are you given the hypotenuse? ", yes_no, "Please enter yes or no.")
+            
             if getting_hyp == "yes":
+                # use pythagoras to calculate missing side
                 side_1 = num_check("Length of side 1: ", "Please enter a number above 0.")
                 side_2 = num_check("Length of side 2: ", "Please enter a number above 0.")
-                desired_side = ((side_1 ** 2) + (side_2 ** 2)) ** 0.5
+                desired_result = ((side_1 ** 2) + (side_2 ** 2)) ** 0.5
 
             else:
                 side_1 = num_check("Length of the hypotenuse: ", "Please enter a number above 0.")
                 side_2 = num_check("Length of side 2: ", "Please enter a number above 0 and below {}.".format(side_1))
-                desired_side = ((side_1 ** 2) - (side_2 ** 2)) ** 0.5
+                desired_result = ((side_1 ** 2) - (side_2 ** 2)) ** 0.5
 
         else:
+            # get what is needed to calculate angle and calculate
             what_have_1 = string_checker("Give me your first side: ", side_valid, "Please enter hypotenuse / opposite / adjacent.")
             have_check = True
+            
             while have_check == True:
                 what_have_2 = string_checker("Give me your second side: ", side_valid, "Please enter hypotenuse / opposite / adjacent.")
+                have_list = [what_have_1, what_have_2]
+            
                 if what_have_2 == what_have_1:
                     print("Please enter a different side! (hypotenuse / opposite / adjacent)")
                     continue
+            
+                else:
+                    side_1 = num_check("How long is your {}? ".format(what_have_1), "Please enter a number above 0.")
+                    side_2 = num_check("How long is your {}? ".format(what_have_2), "Please enter a number above 0.")
+            
+                    if "hypotenuse" in have_list and "opposite" in have_list:
+                        desired_result = math.asin(side_1 / side_2)
+            
+                    elif "adjacent" in have_list and "hypotenuse" in have_list:
+                        desired_result = math.acos(side_1 / side_2)
+            
+                    elif "opposite" in have_list and "adjacent" in have_list:
+                        desired_result = math.atan(side_1 / side_2)
+            
+                    else:
+                        print("Something happened. That wasn't supposed to happen.")
+                    
                 break
             
 
     else:
-        trig = True
+        side_invalid = "invalid"
+        while side_invalid == "invalid":
+            what_side = string_checker("What side are you trying to get? ", side_valid, "Please enter a valid side!")
+            what_have_1 = string_checker("What side do you have? ", side_valid, "Please enter a valid side.")
+            if what_have_1 != what_side:
+                break
+            print("Please don't enter the same sides!")
 
+        side_1 = num_check("How long is your side? ", "Please enter a number above 0.")
+        angle_size = num_check("What's the size of your angle", "Please enter a number above 0 and below 90.", 90)
+        
+        if what_side == "hypotenuse":
+        
+            if what_have_1 == "opposite":
+                desired_result = side_1 / math.sin(math.radians(angle_size))
+        
+            elif what_have_1 == "adjacent":
+                desired_result = side_1 / math.cos(math.radians(angle_size))
 
+        elif what_side == "opposite":
+
+            if what_have_1 == "hypotenuse":
+                desired_result = side_1 * math.sin(math.radians(angle_size))
+            
+            elif what_have_1 == "adjacent":
+                desired_result = side_1 * math.tan(math.radians(angle_size))
+        
+        elif what_side == "adjacent":
+
+            if what_have_1 == "hypotenuse":
+                desired_result = side_1 * math.cos(math.radians(angle_size))
+            
+            elif what_have_1 == "opposite":
+                desired_result = side_1 / math.tan(math.radians(angle_size))
+    
+    return desired_result
+
+    
 def trig_calc():
     have_valid = "invalid"
     while have_valid == "invalid":
@@ -242,4 +296,4 @@ yes_no = [
 
 get_answer = trig_answer()
 
-print("Your answer is: {:.2f}".format(trig_answer))
+print("Your answer is: {:.2f}".format(get_answer))
