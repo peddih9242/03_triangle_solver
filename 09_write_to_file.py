6cd608fe308d1d6e0ed74cb60b648d510172edff
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-06-05 13:47:25 +1200

    write to file component complete

--- 09_write_to_file.py
+++ 09_write_to_file.py
@@ -1,11 +1,11 @@
 import pandas
 
-trig_pythag = ["Pythagoras"]
-length_1 = [4]
-length_2 = [3]
-given_angles = ["N/A"]
-which_trig = ["N/A"]
-answer = [5]
+trig_pythag = ["Pythagoras", "Trigonometry"]
+length_1 = [4, 5]
+length_2 = [3, 20.05]
+given_angles = ["N/A", 76]
+which_trig = ["N/A", "tan"]
+answer = [5, 20.05]
 
 results_dict = {
     "Calculation Type": trig_pythag,
@@ -16,11 +16,19 @@ results_dict = {
     "Answer": answer
 }
 
+# create dataframe to write to file
 results_frame = pandas.DataFrame(results_dict)
 results_frame = results_frame.set_index("Calculation Type")
-print(results_frame)
+
+# change dataframe to text for writing to file
+results_txt = pandas.DataFrame.to_string(results_frame)
+
+# set up write to file
 file_name = "triangle_stats.txt"
 text_file = open(file_name, "w+")
-text_file.write("eee")
-text_file.write(results_frame)
+
+# heading
+text_file.write("**** Triangle Solver Stats ****")
+text_file.write("\n\n")
+text_file.write(results_txt)
 text_file.close()
